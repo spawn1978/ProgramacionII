4e2f3becacd4354c2580757fa046cbdbae098002
upszot <upszot@gmail.com>
2018-10-03 19:56:42 -0300

    1er programa

--- 1/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ 1/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,31 +1,79 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
  */
 package javaapplication1;
-
 import java.util.Scanner;
+
 
 /**
  *
  * @author gferrer_mecon
  */
 public class JavaApplication1 {
-
+private static final int TAM = 100;
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        int unNumero;
-        Scanner lector = new Scanner(System.in); 
+        int cant=-1;
+        int sumatoria=0;
+        int [] Nros=new int[TAM];
+                
+        do
+        {            
+            cant++;
+            Nros[cant]=getNro();            
+        }while(Nros[cant]!=0);
         
+        
+        mostrar("El Mayor Nro ingresado es: " + getMayor(Nros) ); 
+        
+    }
+    
+    private static int getMayor(int[] Numeros )
+    {
+        int Mayor=0;
+        for(int i=0;i<TAM;i++)
+        {
+            if( ComparaNros(Mayor,Numeros[i]) == -1 )
+            {
+                Mayor=Numeros[i];
+            }
+        }
+        return Mayor;
+    }
+    
+    private static void mostrar( String sms)
+    {
+        System.out.println(sms);        
+    }
+    
+    private static int ComparaNros( int Nro1, int Nro2)
+    {
+        int retorno=9;
+        if(Nro1>Nro2)
+        {           
+            retorno=1;
+        }
+        if( Nro1==Nro2)
+        {
+            retorno=0;
+        }
+        if(Nro1<Nro2)
+        {
+            retorno=-1;
+        }            
+        return retorno;
+    }
+    
+    public static int getNro()
+    {
+        int Numero;               
+        Scanner lector = new Scanner(System.in);        
         System.out.println("Ingrese un Nro: "); //saco datos por la consola
-        unNumero=lector.nextInt();//tomo datos de la consola
-        System.out.println("El numero es: "+unNumero);
-        
-        
+        Numero=lector.nextInt();//tomo datos de la consola
+        return Numero;
     }
     
 }
---
+++ 1/Readme.txt
@@ -0,0 +1,8 @@
+1. Ingresar 5 numeros enteros por consola, luego calcular y mostrar el valor maximo, minimo y promedio
+2. mostrar por pantalla todos los nros primos que haya hasta un nro que ingrese el usuario pro consola.
+3. crear una funcion estatica que permita devolver el mayor de 2 numeros que recibe por parametros
+4. se ingresan nros (distintos de 0) no se sabe cuantos, se pide:
+ 	a. cantidad de positivos, y negaticos.
+	b. cantidad de pares e impares.
+	c. promedio de los positivos.
+	d. porcentaje de positivos y negativos.
