4539068e55f29379ff96ff1c72ff3b096471e342
upszot <upszot@gmail.com>
2018-10-03 22:31:41 -0300

    lo unico que falta es el calculo de los numeros primos

--- 1/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ 1/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -27,23 +27,137 @@ private static final int TAM = 100;
         }while(Nros[cant]!=0);
         
         
-        mostrar("El Mayor Nro ingresado es: " + getMayor(Nros) ); 
+        mostrar("El Mayor Nro ingresado es: " + getMayor(Nros,cant) ); 
+        mostrar("El Menor Nro ingresado es: " + getMenor(Nros,cant) ); 
+        mostrar("La sumatoria de los nros es: " + getSumatoria(Nros,cant) ); 
+        mostrar("El promedio es: " + getSumatoria(Nros,cant)/cant ); 
+        mostrar("El Cantidad de Nros positivos: " + getCantPositivos(Nros) ); 
+        mostrar("El Cantidad de Nros Negativos: " + getCantNegativos(Nros) ); 
+        mostrar("%Nros Positivos: " + getCantPositivos(Nros) *100 / cant + "%");
+        mostrar("%Nros Negativos: " + getCantNegativos(Nros) *100 / cant + "%");
+        mostrar("Promedio de Nros Positivos: " + getPromedioPositivos(Nros) ); 
+        
+        mostrar("Cantidadde Nros Pares: " + getCantPares(Nros,cant) ); 
+        mostrar("Cantidadde Nros Impares: " + getCantImpares(Nros,cant) ); 
         
     }
     
-    private static int getMayor(int[] Numeros )
+    public static int getCantPares(int [] Numeros,int cantidad)
     {
-        int Mayor=0;
-        for(int i=0;i<TAM;i++)
-        {
-            if( ComparaNros(Mayor,Numeros[i]) == -1 )
+        int retorno=0;
+        for (int i = 0; i < cantidad; i++) {
+            if( esPar(Numeros[i]) == true )
             {
+                retorno ++;
+            }            
+        }
+        return retorno;
+    }
+
+    public static int getCantImpares(int [] Numeros ,int cantidad)
+    {
+        int retorno=0;
+        for (int i = 0; i <cantidad; i++) {
+            if( esPar(Numeros[i]) == false )
+            {
+                retorno ++;
+            }            
+        }
+        return retorno;
+    }
+    
+    private static boolean esPar(int numero)
+    {
+        boolean retorno;
+        if (numero%2==0)
+        {
+            retorno=true;
+        }
+        else
+        {
+            retorno=false;
+        }
+        return retorno;
+    }
+    
+    public static int getSumatoria(int [] Numeros, int cantidad)
+    {
+        int retorno=0;
+        for (int i = 0; i < Numeros.length; i++) {
+            retorno += Numeros[i];            
+        }
+        return retorno;
+    }
+    
+    private static float getPromedioPositivos(int [] Numeros)
+    {
+        float retorno=0;
+        int sumatoria=0;
+        int cantidad=0;
+        for (int i = 0; i < Numeros.length; i++) {
+            if( Numeros[i]>0)
+            {
+                sumatoria+=Numeros[i];
+                cantidad ++;
+            }            
+        }
+        retorno=sumatoria/cantidad;
+        return retorno;
+    }
+    private static int getCantPositivos(int[] Numeros)
+    {
+        int retorno=0;
+        for (int i = 0; i < Numeros.length; i++) {
+            
+            if(Numeros[i]>0)
+            {
+                retorno++;
+            }            
+        }
+        return retorno;
+    }
+    private static int getCantNegativos(int[] Numeros)
+    {
+        int retorno=0;
+        for (int i = 0; i < Numeros.length; i++) {
+            
+            if(Numeros[i]<0)
+            {
+                retorno++;
+            }            
+        }
+        return retorno;
+    }
+    
+    private static int getMayor(int[] Numeros, int cantidad )
+    {
+        int Mayor = 0; //para que no me putee el compilador pero esta de mas...
+        int flag=0; 
+        for(int i=0;i<cantidad;i++)
+        {
+            if( flag==0 || ComparaNros(Mayor,Numeros[i]) == -1 )
+            {
+                flag=1;
                 Mayor=Numeros[i];
             }
         }
         return Mayor;
     }
-    
+    private static int getMenor(int[] Numeros ,int cantidad)
+    {
+        int Menor = 0; //para que no me putee el compilador pero esta de mas...
+        int flag=0; 
+        for(int i=0;i<cantidad;i++)
+        {
+            if( flag==0 || ComparaNros(Menor,Numeros[i]) == 1 )
+            {
+                flag=1;
+                Menor=Numeros[i];
+            }
+        }
+        return Menor;
+    }
+        
     private static void mostrar( String sms)
     {
         System.out.println(sms);        
--- 1/Readme.txt
+++ 1/Readme.txt
@@ -1,8 +1,11 @@
-1. Ingresar 5 numeros enteros por consola, luego calcular y mostrar el valor maximo, minimo y promedio
+OK - 1. Ingresar 5 numeros enteros por consola, luego calcular y mostrar el valor maximo, minimo y promedio
+
 2. mostrar por pantalla todos los nros primos que haya hasta un nro que ingrese el usuario pro consola.
-3. crear una funcion estatica que permita devolver el mayor de 2 numeros que recibe por parametros
-4. se ingresan nros (distintos de 0) no se sabe cuantos, se pide:
- 	a. cantidad de positivos, y negaticos.
-	b. cantidad de pares e impares.
-	c. promedio de los positivos.
-	d. porcentaje de positivos y negativos.
+
+OK - 3. crear una funcion estatica que permita devolver el mayor de 2 numeros que recibe por parametros
+
+OK - 4. se ingresan nros (distintos de 0) no se sabe cuantos, se pide:
+OK 	a. cantidad de positivos, y negaticos.
+OK	b. cantidad de pares e impares.
+OK	c. promedio de los positivos.
+OK	d. porcentaje de positivos y negativos.
