d8d93b8b0282f19cf291684d8f5c6c4dbf324539
upszot <upszot@gmail.com>
2018-11-16 17:52:55 -0300

    1er parcial

---
+++ 1_Parcial/FerrerGonzalo/src/ferrergonzalo/Biblioteca.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ferrergonzalo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gferrer_mecon
+ */
+public class Biblioteca
+{
+    private int _capacidad;
+    private ArrayList <Libro> _libros;
+
+    //<editor-fold desc="CONSTRUCTOR">
+        public Biblioteca()
+        {                
+            this._libros = new ArrayList<>();       
+        }
+
+        public Biblioteca( int CapacidadMaxima)
+        {                
+            this();
+            this._capacidad=CapacidadMaxima;
+        } 
+    //</editor-fold>
+        
+    //<editor-fold desc="METODO">
+        private boolean estaEnBiblioteca(Libro libroBuscar)
+        {
+            boolean retorno=false;
+            for (Libro unLibro : _libros)
+            {
+                if(Libro.compararLibros(unLibro, libroBuscar))
+                {
+                    retorno=true;
+                    break;
+                }
+            }
+            return retorno;
+        }
+        
+    //</editor-fold>
+}
